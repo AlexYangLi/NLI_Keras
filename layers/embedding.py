283875d635d36e235eb7e513c95350ff7639a52d
alex_yang <2041462766@qq.com>
2019-03-12 15:25:44 +0800

    fix bug: not consider padded index

--- layers/embedding.py
+++ layers/embedding.py
@@ -28,7 +28,7 @@ class ELMoEmbedding(Layer):
                 https://github.com/JHart96/keras_elmo_embedding_layer/blob/master/elmo.py
                 https://tfhub.dev/google/elmo/2
     """
-    def __init__(self, output_mode, idx2word=None, max_length=None, mask_zero=False, hub_url=None, trainable=None,
+    def __init__(self, output_mode, idx2word=None, max_length=None, mask_zero=False, hub_url=None, elmo_trainable=None,
                  **kwargs):
         """
         inputs to ELMoEmbedding can be untokenzied sentences (shaped [batch_size, 1], typed string) or tokenzied word's
@@ -48,7 +48,7 @@ class ELMoEmbedding(Layer):
         self.word_mapping = None
         self.lookup_table = None
 
-        # load elmo model locally by providing a local path due to the huge delay downloading the model
+        # load elmo model locally by providing a local path due to the huge delay of downloading the model
         # for more information, see:
         # https://stackoverflow.com/questions/50322001/how-to-save-load-a-tensorflow-hub-module-to-from-a-custom-path
         # https://www.tensorflow.org/hub/hosting
@@ -56,8 +56,8 @@ class ELMoEmbedding(Layer):
             self.hub_url = hub_url
         else:
             self.hub_url = 'https://tfhub.dev/google/elmo/2'
-        if trainable is not None:
-            self.trainable = trainable
+        if elmo_trainable is not None:
+            self.trainable = elmo_trainable
         else:
             self.trainable = True if self.output_mode == 'elmo' else False
         self.elmo = None
@@ -72,15 +72,17 @@ class ELMoEmbedding(Layer):
             self.input_type = 'word_id'
             self.max_length = input_shape[1]
             assert self.idx2word is not None
+            self.idx2word[0] = ''   # padded position, must add
             self.word_mapping = [x[1] for x in sorted(self.idx2word.items(), key=lambda x: x[0])]
             self.lookup_table = tf.contrib.lookup.index_to_string_table_from_tensor(self.word_mapping,
                                                                                     default_value="<UNK>")
             self.lookup_table.init.run(session=K.get_session())
 
-        print('load elmo from tensorflow hub....')
+        print('Logging Info - Loading elmo from tensorflow hub....')
         self.elmo = hub.Module(self.hub_url, trainable=self.trainable, name="{}_elmo_hub".format(self.name))
 
-        self.trainable_weights += K.tf.trainable_variables(scope="^{}_elmo_hub/.*".format(self.name))
+        if self.trainable:
+            self.trainable_weights += K.tf.trainable_variables(scope="^{}_elmo_hub/.*".format(self.name))
 
     def call(self, inputs, mask=None):
         if self.input_type == 'sentence':
@@ -101,10 +103,15 @@ class ELMoEmbedding(Layer):
             embeddings = self.elmo(inputs={'tokens': self.lookup_table.lookup(inputs),
                                            'sequence_len': sequence_lengths},
                                    signature="tokens", as_dict=True)[self.output_mode]
+            if self.output_mode != 'defalut':
+                output_mask = K.expand_dims(K.not_equal(inputs, 0), axis=-1)
+                embeddings *= output_mask
+
         return embeddings
 
     def compute_mask(self, inputs, mask=None):
-        if not self.mask_zero or self.input_type == 'sentence':     # hard to compute mask when using sentences as input
+        if not self.mask_zero or self.input_type == 'sentence' or self.output_mode == 'default':
+            # hard to compute mask when using sentences as input
             return None
         output_mask = K.not_equal(inputs, 0)
         return output_mask
